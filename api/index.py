8c0b9e0863d7a7c9289cb5d35b0ad2e9bd736b46
Adri Fernandez <128298030+adrifernandev@users.noreply.github.com>
2024-02-05 06:38:05 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+
+from Split import Split  
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return 'Hello, World!'
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/balance', methods=['POST'])
+def balance():
+    data = request.json
+    if not data or 'expenses' not in data:
+        return jsonify({'error': 'Bad request, missing expenses data'}), 400
+
+    expenses = data['expenses']
+    split_instance = Split(expenses)
+    transactions = split_instance.balance()
+
+    formatted_transactions = [f"{trans[0]}, {trans[1]}, {trans[2]}" for trans in transactions]
+
+    return jsonify({"balance": formatted_transactions})
+
+
+if __name__ == '__main__':
+    app.run()
